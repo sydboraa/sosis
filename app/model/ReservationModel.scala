c48690ec22d8871fc07405b721b54c051b393d2e
sydboraa <seydabora90@gmail.com>
2015-07-06 13:33:06 +0300

    reservationModel created!

--- app/model/ReservationModel.scala
+++ app/model/ReservationModel.scala
@@ -1,5 +1,7 @@
 package model
 
+import java.util.Date
+
 import play.api.libs.functional.syntax._
 import play.api.libs.json.{Writes, JsPath, Reads}
 /**
@@ -7,19 +9,31 @@ import play.api.libs.json.{Writes, JsPath, Reads}
  */
 object ReservationModel {
 
-  case class Reservation(var id: Option[Long], firstName: String, middleName: Option[String], lastName: String) {
-    override def toString() = s"id : $id "
-  }
+  case class Reservation(id: Int, restaurantId: Int, seatId: Int, reservationCode: String, data: String, dataVersion: Int, price: Double, isArchived:Int, isActive:Int, cDate:Date, uDate:Date)
 
-  implicit val aliasReads: Reads[Reservation] = (
-    (JsPath \ "id").readNullable[Long] and
-      (JsPath \ "firstName").read[String] and
-      (JsPath \ "middleName").readNullable[String]
-      and (JsPath \ "lastName").read[String])(Reservation.apply _)
+  implicit val reservationReads: Reads[Reservation] = (
+    (JsPath \ "id").read[Int] and
+      (JsPath \ "restaurantId").read[Int] and
+      (JsPath \ "seatId").read[Int] and
+      (JsPath \ "reservationCode").read[String] and
+      (JsPath \ "data").read[String] and
+      (JsPath \ "dataVersion").read[Int] and
+      (JsPath \ "price").read[Double] and
+      (JsPath \ "isArchived").read[Int] and
+      (JsPath \ "isActive").read[Int] and
+      (JsPath \ "cDate").read[Date] and
+      (JsPath \ "uDate").read[Date])(Reservation.apply _)
 
-  implicit val aliasWrites: Writes[Reservation] = (
-    (JsPath \ "id").writeNullable[Long] and
-      (JsPath \ "firstName").write[String] and
-      (JsPath \ "middleName").writeNullable[String] and
-      (JsPath \ "lastName").write[String])(unlift(Reservation.unapply))
+  implicit val reservationWrites: Writes[Reservation] = (
+    (JsPath \ "id").write[Int] and
+      (JsPath \ "restaurantId").write[Int] and
+      (JsPath \ "seatId").write[Int] and
+      (JsPath \ "reservationCode").write[String] and
+      (JsPath \ "data").write[String] and
+      (JsPath \ "dataVersion").write[Int] and
+      (JsPath \ "price").write[Double] and
+      (JsPath \ "isArchived").write[Int] and
+      (JsPath \ "isActive").write[Int] and
+      (JsPath \ "cDate").write[Date] and
+      (JsPath \ "uDate").write[Date])(unlift(Reservation.unapply))
 }
